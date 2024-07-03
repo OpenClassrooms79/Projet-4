1c9266cff86538b5e355e2efe0c3e0c253e1711c
Jocelyn Flament <jocelyn.flament@gmail.com>
2024-07-03 10:36:57 +0200

    ajout fonction d'insertion d'une oeuvre dans la base de données

--- traitement.php
+++ traitement.php
@@ -1,5 +1,17 @@
 <?php
-/**
- * @author Jocelyn Flament
- * @since 03/07/2024
- */
+function ajouterOeuvre()
+{
+    if (empty($_POST['titre']) || empty($_POST['artiste']) || empty($_POST['description']) || empty($_POST['image'])
+        || (strlen($_POST['description']) < 3) || !filter_var($_POST['image'], FILTER_VALIDATE_URL)) {
+        header('Location: index.php');
+    }
+
+    // insertion dans la table 'oeuvres'
+    $mysql = connexion();
+    $mysql->prepare('INSERT INTO oeuvres(titre, artiste, description, image) VALUES (:titre, :artiste, :description, :image)')->execute([
+        'titre' => $_POST['titre'],
+        'artiste' => $_POST['artiste'],
+        'description' => $_POST['description'],
+        'image' => $_POST['image'],
+    ]);
+}
